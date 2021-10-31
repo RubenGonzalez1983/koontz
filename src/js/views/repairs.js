ad787314a5ddbdba717504e6d65c5812b93f0d85
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-10-31 18:11:25 +0000

    gramatical errors fixed

--- src/js/views/repairs.js
+++ src/js/views/repairs.js
@@ -11,22 +11,22 @@ export const Repairs = () => (
 				<h1 className="underline">Set-Up and Fret Level</h1>
 				<br />
 				<p>
-					Full Set-Up includes: Inspection of instrument and electronics, Personalized recommendations for
-					upgrades and improvements, Cleaning and oiling of fretboard, Tighten all hardware, Re-String, Adjust
-					Truss Rod, Adjust Bridge height, Set intonation, Check and clean electronics.
+					Full Set-Up includes: Inspection of instrument and electronics, personalized recommendations for
+					upgrades and improvements, cleaning and oiling of fretboard, tighten all hardware, restring, adjust
+					truss rod, adjust bridge height, set intonation, check and clean electronics.
 				</p>
 				<br />
 				<p>
 					{" "}
-					Fret Level includes: Measuring and precision leveling of frets using the Erlewine Neck Jig, ReCrown
+					Fret Level includes: measuring and precision leveling of frets using the Erlewine Neck Jig, recrown
 					fret tops, polish all frets.
 				</p>{" "}
 				<br />
 				<h3>Pricing for Standard Guitars</h3>
 				<p>
 					{" "}
-					Hardtails, Les Pauls, Teles, Strat style guitars, Acoustic and Classical Guitars, Bigsby Guitars, 7
-					strings, 4 or 5 string basses, Acoustic Basses.
+					Hardtails, Les Pauls, Teles, Strat style guitars, acoustic and classical Guitars, Bigsby Guitars, 7
+					strings, 4 or 5 string basses, acoustic basses.
 				</p>{" "}
 				<br />
 				<ul>Regular Set-up ---- $70</ul>
@@ -35,12 +35,12 @@ export const Repairs = () => (
 				<h3>Pricing for Other Guitars</h3>
 				<p>
 					{" "}
-					Locking Tremolo Guitars (Floyd Rose), 12 String Guitars, 6 String Basses, Banjos, Jaguars and
+					Locking tremolo guitars (Floyd Rose), 12 string guitars, 6 string basses, banjos, Jaguars and
 					Jazzmasters.
 					<br />
 				</p>
-				<ul>Full Set-Up ---- $80</ul>
-				<ul>Set-Up with fret level ---- $160</ul>
+				<ul>Full set-up ---- $80</ul>
+				<ul>Set-up with fret level ---- $160</ul>
 				<br /> <br />
 				<h1 className="underline">Crack and Structural Repairs</h1> <br />
 				<ul>
@@ -49,21 +49,21 @@ export const Repairs = () => (
 					<li>Seal crack up to 6” ---- $100-$150</li>
 					No Finish <br /> <br />
 					<li>Repair Crack up to 3” ---- $150-200</li>
-					With finish touch up Plus materials($10) Over 6” must quote. <br />
+					With finish touch up plus materials($10) Over 6” must quote. <br />
 					<br />
-					<li>Seal Cracked Bridge ---- $25-75</li> <br />
-					<li>Headstock ReGlue ---- $200-250</li>
-					No touch up, No Splines <br /> <br />
+					<li>Seal Cracked bridge ---- $25-75</li> <br />
+					<li>Headstock reglue ---- $200-250</li>
+					No touch up, no splines <br /> <br />
 					<li>Headstock Reglue ---- $300-350</li>
-					w/Spline reinforcements, No touchup W/touch up add $50-75 <br /> <br />
-					<li>Reglue Loose brace ---- $50</li>
+					w/spline reinforcements, no touchup w/touch up add $50-75 <br /> <br />
+					<li>Reglue loose brace ---- $50</li>
 					$30 each additional brace. <br />
 					<br />
 				</ul>{" "}
 				<br />
 				<h1 className="underline">Nuts</h1> <br />
 				<p>
-					<i>Different types of neck repairs from Nuts to Frets. For headstock crack see Crack Repair</i>
+					<i>Different types of neck repairs from nuts to frets. For headstock crack see crack repair</i>
 				</p>
 				<ul>
 					<li>Fender style bone nut ---- $90</li>
@@ -72,7 +72,7 @@ export const Repairs = () => (
 					Made from scratch. <br /> <br />
 					<li>12 String bone nut ---- $150</li>
 					Made from scratch. <br /> <br />
-					<li>Install Synthetic Nut ---- $50</li>
+					<li>Install synthetic nut ---- $50</li>
 					Plus cost of nut. <br /> <br />
 					<li>Brass nut ---- $150</li>{" "}
 				</ul>
@@ -80,13 +80,13 @@ export const Repairs = () => (
 				<h1 className="underline">Frets and Fingerboards</h1> <br />
 				<ul>
 					<li>Full Fret level and recrown ---- $100</li> <br />
-					<li>Fret end File ---- $50</li> <br />
+					<li>Fret end file ---- $50</li> <br />
 					<li>Fret level, recrown, fret ends ---- $125</li> <br />
-					<li>ReCrown only ---- $40</li> <br />
-					<li>Re-Seat fret ---- $15 a fret</li> <br />
-					<li>Full Re Fret ---- $350</li>
-					Unbound Maple or Rosewood neck. <br /> <br />
-					<li>Re Fret Bound Neck ---- 450</li>{" "}
+					<li>Recrown only ---- $40</li> <br />
+					<li>Reseat fret ---- $15 a fret</li> <br />
+					<li>Full refret ---- $350</li>
+					Unbound maple or rosewood neck. <br /> <br />
+					<li>Refret bound neck ---- 450</li>{" "}
 				</ul>
 				<br />
 				<br />
