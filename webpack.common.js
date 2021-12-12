1bb645e554d18cf83d7068e0b3319be7961483e2
RubenGonzalez1983 <gonzalezjuarez83@gmail.com>
2021-12-12 21:29:56 +0000

    changed ico

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
       Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
     }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        favicon: 'koontz.ico',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
